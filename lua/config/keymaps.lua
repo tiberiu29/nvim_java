7d9cc866714c24979dd72fb905ab629c1cd8db7f
Tiberiu <gatatiberiucalin@gmail.com>
2026-06-11 20:32:58 +0300

    Improvement: map back/forward moving as intellij

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,22 +1,51 @@
-
 -- Setup <leader> in keymapping
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", {desc = "Remove search highlights"})
-vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", {desc = "Exit terminal mode"})
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", {
+    desc = "Remove search highlights"
+})
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", {
+    desc = "Exit terminal mode"
+})
 
 -- Navigate between windows. Ctrl + h/l/j/k
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", {desc = "Move focus to left window"})
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", {desc = "Move focus to right window"})
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", {desc = "Move focus to lower window"})
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", {desc = "Move focus to upper window"})
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", {
+    desc = "Move focus to left window"
+})
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", {
+    desc = "Move focus to right window"
+})
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", {
+    desc = "Move focus to lower window"
+})
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", {
+    desc = "Move focus to upper window"
+})
 
 -- Split windows
-vim.keymap.set("n", "<leader>wv", ":vsplit<cr>", {desc = "[W]indow Split [V]ertical"})
-vim.keymap.set("n", "<leader>wh", ":split<cr>", {desc = "[W]indow Split [H]orizontal"})
+vim.keymap.set("n", "<leader>wv", ":vsplit<cr>", {
+    desc = "[W]indow Split [V]ertical"
+})
+vim.keymap.set("n", "<leader>wh", ":split<cr>", {
+    desc = "[W]indow Split [H]orizontal"
+})
 
 -- Indent left/right in view mode. 
-vim.keymap.set("v", "<", "<gv", {desc = "Indent left in visual mode"})
-vim.keymap.set("v", ">", ">gv", {desc = "Indent left in visual mode"})
+vim.keymap.set("v", "<", "<gv", {
+    desc = "Indent left in visual mode"
+})
+vim.keymap.set("v", ">", ">gv", {
+    desc = "Indent left in visual mode"
+})
+
+-- Go to previous location
+vim.keymap.set("n", "<C-M-Left>", "<C-o>", {
+    desc = "Jump Back"
+})
+
+-- Go to next location
+vim.keymap.set("n", "<C-M-Right>", "<C-i>", {
+    desc = "Jump Forward"
+})
 
