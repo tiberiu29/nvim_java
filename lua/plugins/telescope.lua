408966bc1b00514a7d03cf9b210349ab2738dc21
Tiberiu <gatatiberiucalin@gmail.com>
2026-06-11 21:04:42 +0300

    Improvement: expand behavior of telescope to simplify querying jdtl paths

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,47 +1,80 @@
-return {
-    {
-        'nvim-telescope/telescope.nvim',
-        dependencies = {
-            'nvim-lua/plenary.nvim'
-        },
-        config = function()
-            -- Get access to telescope functions
-            local builtin = require('telescope.builtin')
+return {{
+    'nvim-telescope/telescope.nvim',
+    dependencies = {'nvim-lua/plenary.nvim'},
+    config = function()
+        local telescope = require("telescope")
+        local builtin = require("telescope.builtin")
+        local actions = require("telescope.actions")
+        local layout = require("telescope.actions.layout")
 
-            -- Search files by name
-            vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = "[F]ind [F]iles"})
-            -- Search for files containing text
-            vim.keymap.set('n', '<leader>fg', builtin.live_grep, {desc = "[F]ind by [G]rep"})
-            -- Search for code diagnostics
-            vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {desc = "[F]ind [D]iagnostics"})
-            -- Resume to previous search
-            vim.keymap.set('n', '<leader>fr', builtin.resume, {desc = "[F]inder [R]esume"})
-            -- Search for recent files
-            vim.keymap.set('n', '<leader>f.', builtin.oldfiles, {desc = "[F]ind [R]ecent files"})
-            -- Search open buffers
-            vim.keymap.set('n', '<leader>fb', builtin.buffers, {desc = "[F]ind existing [B]uffers"})
-        end
-    }, 
-    {
-        'nvim-telescope/telescope-ui-select.nvim',
-        config = function()
-            local actions = require("telescope.actions")
-            require("telescope").setup({
-                extensions = {
-                    ["ui-select"] = {
-                        require("telescope.themes").get_dropdown{}
-                    }
+        telescope.setup({
+            defaults = {
+                layout_strategy = "horizontal",
+                layout_config = {
+                    width = 0.95,
+                    height = 0.95,
+                    preview_width = 0.45
                 },
                 mappings = {
                     i = {
                         ["<C-n>"] = actions.cycle_history_next,
                         ["<C-p>"] = actions.cycle_history_prev,
+
+                        -- expand Results to max / restore Preview
+                        ["<C-Right>"] = layout.toggle_preview,
+                        ["<C-Left>"] = layout.toggle_preview,
+
+                        -- fallback if terminal does not send Ctrl+Arrow
+                        ["<M-p>"] = layout.toggle_preview
+                    },
+                    n = {
+                        ["<C-Right>"] = layout.toggle_preview,
+                        ["<C-Left>"] = layout.toggle_preview,
+                        ["<M-p>"] = layout.toggle_preview
                     }
-                },
-                require("telescope").load_extension("ui-select")
-            })
-        end
-    }
+                }
+            },
+            extensions = {
+                ["ui-select"] = {require("telescope.themes").get_dropdown({})}
+            }
+        })
 
+        telescope.load_extension("ui-select")
 
-}
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, {
+            desc = "[F]ind [F]iles"
+        })
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {
+            desc = "[F]ind by [G]rep"
+        })
+        vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {
+            desc = "[F]ind [D]iagnostics"
+        })
+        vim.keymap.set('n', '<leader>fr', builtin.resume, {
+            desc = "[F]inder [R]esume"
+        })
+        vim.keymap.set('n', '<leader>f.', builtin.oldfiles, {
+            desc = "[F]ind [R]ecent files"
+        })
+        vim.keymap.set('n', '<leader>fb', builtin.buffers, {
+            desc = "[F]ind existing [B]uffers"
+        })
+    end
+}, {
+    'nvim-telescope/telescope-ui-select.nvim',
+    config = function()
+        local actions = require("telescope.actions")
+        require("telescope").setup({
+            extensions = {
+                ["ui-select"] = {require("telescope.themes").get_dropdown {}}
+            },
+            mappings = {
+                i = {
+                    ["<C-n>"] = actions.cycle_history_next,
+                    ["<C-p>"] = actions.cycle_history_prev
+                }
+            },
+            require("telescope").load_extension("ui-select")
+        })
+    end
+}}
